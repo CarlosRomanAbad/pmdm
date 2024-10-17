2874e6464c4445cb0e05ad90e0d4d1caf1316571
carlitosromvn <carlosromanabad@gmail.com>
2024-10-17 16:36:57 +0200

    done

--- AngularProjects/progress-bar-angular/src/app/app.component.ts
+++ AngularProjects/progress-bar-angular/src/app/app.component.ts
@@ -10,4 +10,21 @@ import { ProgressBarComponent } from './components/progress-bar/progress-bar.com
 export class AppComponent {
 [x: string]: any;
   title = 'progress-bar-angular';
+
+  valor: number = 50;
+
+comprobarValor(valor: number) : void{
+  if(valor < 0){
+    this.valor = 0;
+    
+  }
+  else if(valor > 100){
+    this.valor = 100;
+  }
+  else{
+    this.valor = valor;
+  }
+  
+}
+
 }
