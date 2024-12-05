5246acdc49a3072ba73af5dc6c7df4a8ae773144
carlitosromvn <carlosromanabad@gmail.com>
2024-12-05 19:11:46 +0100

    refactor: Update login interface layout and styles
    The code changes in `login_interface.dart` update the layout and styles of the login interface. The changes include updating the background color, centering the image and text, adjusting the button styles, and adding spacing between elements. This refactor improves the visual appeal and user experience of the login interface.

--- flotter/ui_login/lib/login_interface.dart
+++ flotter/ui_login/lib/login_interface.dart
@@ -6,56 +6,97 @@ class LoginInterface extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(230, 47, 22, 1.0),
-      appBar: AppBar(
-        title: const Text('Login Interface'),
-      ),
+      backgroundColor: const Color(0xFFE62F16), 
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const Image(
-              image: NetworkImage('https://lh3.googleusercontent.com/drive-storage/AJQWtBM2NF6UI-6aDgbvwlLUQrk-kuZ7xOIq0y0UIARlKH0sd32w0yxQBbTHB_Vb7mKvLqvMLgZJx3qmBf1c60qkQSW6nopZwa1rJvhfdNoae2oTSQ=w1903-h925'),
-              width: 200,
-              
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Beautiful, Private Sharing',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.normal,
-                color: Color.fromARGB(108, 255, 255, 255),
+            const Spacer(),
+
+            const Center(
+              child: Image(
+                image: NetworkImage('https://lh3.googleusercontent.com/drive-storage/AJQWtBPCwRFmb73s_ykcLmzaXbiug7tltMjZQqnLPyaw6QHgcoiZGxzG3C-qk1H_OgrSZWqvma5KjFASAfw-RiHmuaPbjz2CtfPBM1shvu4R2TwGcRc=w2940-h1680'),
+                width: 250,
+                
               ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Sign Up'),
-              
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Password',
-                icon: const Icon(Icons.lock),
-                iconColor: const Color.fromARGB(125, 212, 191, 191),
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+            const SizedBox(height: 10),
+
+            // Subtítulo
+            const Center(
+              child: Text(
+                'Beautiful, Private Sharing',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white70,
                 ),
               ),
-              obscureText: true,
             ),
-            const SizedBox(height: 20),
+            const Spacer(),
+
+
             ElevatedButton(
               onPressed: () {},
-              child: const Text('Login'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: const Color(0xFFE62F16),
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              child: const Text(
+                'Sign Up',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
             ),
             const SizedBox(height: 20),
-           
+
+
+            const Center(
+              child: Text(
+                'Already have a Path account?',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white70,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+
+            // Botón "Login"
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                foregroundColor: const Color.fromARGB(100, 255, 255, 255),
+                side: const BorderSide(color: Color.fromARGB(137, 255, 255, 255), width: 1.5),
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              child: const Text(
+                'Login',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            // Términos y condiciones
+            const Center(
+              child: Text(
+                'By using Path, you agree to Path\'s\nTerms of Use and Privacy Policy.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.white70,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
           ],
         ),
       ),
